1e2f4b5cd2cbac222fb43631715ac7be5398299b
Mehmet Sonmez <101952360+Memo-Lee@users.noreply.github.com>
2022-09-29 13:26:21 +0300

    Add files via upload

---
+++ CrowdFund.sol
@@ -0,0 +1,149 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// IERC20 dosyası import
+// import "./IERC20.sol";
+
+interface IERC20 {
+    function transfer(address, uint) external returns (bool);
+
+    function transferFrom(
+        address,
+        address,
+        uint
+    ) external returns (bool);
+}
+
+contract CrowdFund{
+    // eventler daha sonra çağırılmak için kullanılır örn: fronted 
+    event Launch(
+        uint id,
+        address indexed creator,
+        uint goal,
+        uint32 startAt,
+        uint32 endAt
+    );
+
+    event Cancel(uint id);
+    event Pledge(uint indexed id, address indexed caller, uint amount);    
+    event Unpledge(uint indexed id, address indexed caller, uint amount);    
+    event Claim(uint id);
+    event Refund(uint indexed id, address indexed caller, uint amount);    
+
+
+    struct Campaign {
+        // kampanya yaratıcısı
+        address creator;
+        // yükseltilecek tokens miktarı
+        uint goal;
+        // Taahhüt edilen toplam tutar
+        uint pledged;
+        // Kampanya başlangıç tarihi
+        uint32 startAt;
+        // Kampanya bitiş tarihi
+        uint32 endAt;
+        // Hedefe ulaşıldıysa ve tokenslar talet edildiyse bool true döndürür.
+        bool claimed;
+    }
+    IERC20 public immutable token;
+    // Oluşturulan toplam kampanya sayısı.
+    uint public count;
+    // // id'den Kampanyaya eşleme
+    mapping(uint => Campaign) public campaigns;
+    // Kampanya kimliğinden eşleme => taahhüt veren => taahhüt edilen miktar
+    mapping(uint => mapping(address => uint)) public pledgedAmount;
+
+    // constructor : contract deploy edilirken 1 kere kullanılır.
+    constructor(address _token){
+        token = IERC20(_token);
+    }
+
+
+   function launch(uint _goal, uint32 _startAt, uint32 _endAt ) external {
+       // require kontrolleri
+       require(_startAt >= block.timestamp , "start at < now");
+       require(_endAt >= _startAt , "end at < start at");
+       require(_endAt <= block.timestamp + 90 days , "end at < max duration");
+
+        count += 1;
+        campaigns[count] = Campaign({
+            // creator : gönderen kişinin adresi
+            creator : msg.sender,
+            goal : _goal,
+            pledged : 0,
+            startAt : _startAt,
+            endAt : _endAt,
+            claimed : false 
+        });
+
+        emit Launch(count, msg.sender, _goal , _startAt, _endAt);
+   }
+    // çıkış işlemi
+    function cancel(uint _id) external {
+        Campaign memory campaign = campaigns[_id];
+        require(msg.sender == campaign.creator, "not creator");
+        require(block.timestamp < campaign.startAt, "started");
+        delete campaigns[_id];
+        emit Cancel(_id);
+    }
+    
+    function pledge(uint _id, uint _amount) external {
+        Campaign storage campaign = campaigns[_id];
+        require(block.timestamp >= campaign.startAt, "not started");
+        require(block.timestamp <= campaign.endAt, "ended");
+        
+        campaign.pledged += _amount;
+        pledgedAmount[_id][msg.sender] += _amount;
+        token.transferFrom(msg.sender, address(this), _amount);
+
+        emit Pledge(_id , msg.sender, _amount);
+    }
+    
+    function unpledge(uint _id, uint _amount) external {
+        Campaign storage campaign = campaigns[_id];
+        require(block.timestamp <= campaign.endAt, "ended");
+        
+        campaign.pledged -= _amount;
+        pledgedAmount[_id][msg.sender] -= _amount;
+        token.transfer(msg.sender, _amount);
+
+        emit Unpledge(_id , msg.sender, _amount);
+    }
+    
+    function claim(uint _id) external {
+        Campaign storage campaign = campaigns[_id];
+        require(msg.sender == campaign.creator, "not creator");
+        require(block.timestamp > campaign.endAt, "not ended");
+        require(campaign.pledged >= campaign.goal, "pledged <= goal");
+        require(!campaign.claimed, "claimed");
+
+        campaign.claimed = true;
+        token.transfer(msg.sender, campaign.pledged);
+
+        emit Claim(_id);
+    }
+
+    function refund(uint _id) external {
+        Campaign storage campaign = campaigns[_id];
+        require(block.timestamp > campaign.endAt, "not ended");
+        require(campaign.pledged < campaign.goal, "pledged < goal");
+
+        uint bal = pledgedAmount[_id][msg.sender];
+        pledgedAmount[_id][msg.sender] = 0;
+        token.transfer(msg.sender, bal);
+
+        emit Refund(_id, msg.sender, bal);
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
